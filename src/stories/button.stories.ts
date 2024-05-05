5eef517659cc3e310807975ab2434f325f051e5c
Demerzel <demerzel.olivaw@gmail.com>
2024-05-05 02:27:53 -0400

    Refactor button variant definitions and classes
    This commit refactors button styles to use BEM syntax instead of nested class syntax, and subsequently refactors the stories to support it.

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -38,29 +38,111 @@ export default meta;
 
 type Story = StoryObj<ButtonArgs>;
 
-const badgeVariants = cva({
+const buttonVariants = cva({
   base: "button",
   variants: {
     color: {
-      primary: "primary",
-      danger: "danger",
-      neutral: "neutral",
-      success: "success",
+      primary: [],
+      danger: [],
+      neutral: [],
+      success: [],
     },
 
     variant: {
-      solid: "solid",
-      outlined: "outlined",
-      tonal: "tonal",
-      text: "text",
+      solid: [],
+      outlined: [],
+      tonal: [],
+      text: [],
     },
 
     size: {
-      default: "default",
-      large: "large",
-      small: "small",
+      default: "button--default",
+      large: "button--large",
+      small: "button--small",
     },
   },
+  compoundVariants: [
+    {
+      variant: "solid",
+      color: "primary",
+      className: "button--primary-solid",
+    },
+    {
+      variant: "solid",
+      color: "success",
+      className: "button--success-solid",
+    },
+    {
+      variant: "solid",
+      color: "danger",
+      className: "button--danger-solid",
+    },
+    {
+      variant: "solid",
+      color: "neutral",
+      className: "button--neutral-solid",
+    },
+    {
+      variant: "outlined",
+      color: "primary",
+      className: "button--primary-outlined",
+    },
+    {
+      variant: "outlined",
+      color: "success",
+      className: "button--success-outlined",
+    },
+    {
+      variant: "outlined",
+      color: "danger",
+      className: "button--danger-outlined",
+    },
+    {
+      variant: "outlined",
+      color: "neutral",
+      className: "button--neutral-outlined",
+    },
+    {
+      variant: "tonal",
+      color: "primary",
+      className: "button--primary-tonal",
+    },
+    {
+      variant: "tonal",
+      color: "success",
+      className: "button--success-tonal",
+    },
+    {
+      variant: "tonal",
+      color: "danger",
+      className: "button--danger-tonal",
+    },
+    {
+      variant: "tonal",
+      color: "neutral",
+      className: "button--neutral-tonal",
+    },
+    {
+      variant: "text",
+      color: "primary",
+      className: "button--primary-text",
+    },
+    {
+      variant: "text",
+      color: "success",
+      className: "button--success-text",
+    },
+    {
+      variant: "text",
+      color: "danger",
+      className: "button--danger-text",
+    },
+    {
+      variant: "text",
+      color: "neutral",
+      className: "button--neutral-text",
+    },
+  ],
 });
 
 export const Primary: Story = {
@@ -69,7 +151,7 @@ export const Primary: Story = {
 
     button.innerText = args.label;
     button.disabled = args.disabled;
-    button.className = badgeVariants({
+    button.className = buttonVariants({
       variant: args.variant,
       color: args.color,
       size: args.size,
