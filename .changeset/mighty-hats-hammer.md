dd89216184e96b5ce4c7e76bb7b4d05561dd258e
Dark Brandon <demerzel.olivaw@gmail.com>
2024-07-10 22:26:20 -0400

    Improved navbar component. Misc fixes for button, card. Added typography utils (#19)
    This CR improves the Navbar API. It also removes padding for the button and navbar icon-only variants. It fixes Card's prominent variant. Finally, it adds a few typography utils.

---
+++ .changeset/mighty-hats-hammer.md
@@ -0,0 +1,5 @@
+---
+"@destinygg/libstiny": minor
+---
+
+Improved navbar component. Misc fixes for button and card. Added typography utils
--- docs/stories/navbar.stories.tsx
+++ docs/stories/navbar.stories.tsx
@@ -13,33 +13,85 @@ export default meta;
 
 type Story = StoryObj<NavbarArgs>;
 
+const Icon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.3337 3.33319C18.3337 3.33319 17.7503 5.08319 16.667 6.16652C18.0003 14.4999 8.83366 20.5832 1.66699 15.8332C3.50033 15.9165 5.33366 15.3332 6.66699 14.1665C2.50033 12.9165 0.416992 7.99986 2.50033 4.16652C4.33366 6.33319 7.16699 7.58319 10.0003 7.49986C9.25033 3.99986 13.3337 1.99986 15.8337 4.33319C16.7503 4.33319 18.3337 3.33319 18.3337 3.33319Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const Menu = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.33301 10H16.6663M3.33301 5H16.6663M3.33301 15H16.6663"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 export const Primary: Story = {
   render: (args) => (
     <div className="navbar">
-      <div
+      <button className="button button--tertiary button--icon-only">
+        <Menu />
+      </button>
+
+      <img
+        src="https://cdn.omniliberal.dev/3.11.1/img/destiny-logo.png"
         className="navbar__logo"
-        style={{ width: 140, backgroundColor: "blue" }}
       />
 
       <div className="navbar__items">
-        <a className="navbar__item navbar__item--icon-only">i</a>
+        <a className="navbar__icon">
+          <Icon />
+        </a>
+        <a className="navbar__icon">
+          <Icon />
+        </a>
+        <a className="navbar__icon">
+          <Icon />
+        </a>
+        <a className="navbar__icon">
+          <Icon />
+        </a>
+      </div>
+
+      <div className="navbar__items">
         <a className="navbar__item navbar__item--active">Home</a>
         <a className="navbar__item">
           Big Screen <span className="badge badge--danger">Live</span>
         </a>
         <a className="navbar__item">Donate</a>
-        <a className="navbar__item">Community</a>
         <a className="navbar__item">Merch</a>
       </div>
 
-      {args.loggedIn && <div className="navbar__user">Username</div>}
+      <div className="navbar__actions">
+        {args.loggedIn && <div className="navbar__user">Username</div>}
 
-      {!args.loggedIn && (
-        <div className="navbar__actions">
-          <button className="button button--primary-text">Sign In</button>
-          <button className="button button--primary-solid">Sign Up</button>
-        </div>
-      )}
+        {!args.loggedIn && (
+          <button className="button button--primary">Sign In</button>
+        )}
+      </div>
     </div>
   ),
   args: {
